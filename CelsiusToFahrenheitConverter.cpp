aafa187662a841b76aa580bd7c29cbd27ce7d575
Jakob Herpel <moka3156@billl03.(none)>
2015-11-04 18:02:18 +0100

    Implement conversions. TODO: fix conversions

--- CelsiusToFahrenheitConverter.cpp
+++ CelsiusToFahrenheitConverter.cpp
@@ -1,1 +1,15 @@
 #include "CelsiusToFahrenheitConverter.hpp"
+
+#include <string>
+
+double CelsiusToFahrenheitConverter::convert(double inputCelsius) {
+  return inputCelsius * (9/5) + 32;
+}
+
+std::string CelsiusToFahrenheitConverter::toString() const{
+  return "Celsius to Fahrenheit Converter";
+}
+
+void CelsiusToFahrenheitConverter::print() const{
+  std::cout << toString();
+}
--- FahrenheitToCelsiusConverter.cpp
+++ FahrenheitToCelsiusConverter.cpp
@@ -1,1 +1,15 @@
 #include "FahrenheitToCelsiusConverter.hpp"
+
+#include <string>
+
+double FahrenheitToCelsiusConverter::convert(double inputFahrenheit) {
+  return (inputFahrenheit-32) * (5/9);
+}
+
+std::string FahrenheitToCelsiusConverter::toString() const{
+  return "Fahrenheit to Celsius Converter";
+}
+
+void FahrenheitToCelsiusConverter::print() const{
+  std::cout << toString();
+}
