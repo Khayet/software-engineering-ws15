ac468b3b4f6f059c930603062b6629d38a2c78fa
ChrisQuinto <ch.paffrath@gmx.net>
2015-12-15 19:26:47 +0100

    adding constructor/convert function

--- Condecorator.hpp
+++ Condecorator.hpp
@@ -1,10 +1,21 @@
 #ifndef CONDECORATOR_HPP
 #define CONDECORATOR_HPP
 
-class Condecorator {
+class Condecorator : public UnitConverter {
 public:
+    Condecorator(std::shared_ptr<UnitConverter> conpointer) :
+    condec_ptr{conpointer} {}
+
+    virtual double convert(const double inValue) const override {
+        if (condec_ptr == nullptr)
+        {
+            return inValue;
+        }
+        return condec_ptr->convert(inValue);
+    }
 
 private:
+    std::shared_ptr<UnitConverter> condec_ptr;
 };
 
 #endif
