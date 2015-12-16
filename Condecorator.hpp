57a691ed1f7c8877b1e3ed7bc448487bbf5faacb
ChrisQuinto <ch.paffrath@gmx.net>
2015-12-16 19:10:45 +0100

    Delete unnecessary functions in Condecorator

--- Condecorator.hpp
+++ Condecorator.hpp
@@ -16,21 +16,6 @@ public:
         return unit_ptr->convert(inValue);
     }
 
-    virtual UnitConverter* clone() const {
-        return unit_ptr->clone();
-    };
-
-    virtual std::string toString() const {
-        if (unit_ptr == nullptr) {
-            return "ERROR Condecorator::toString(): Missing unit_ptr.";
-        }
-        return unit_ptr->toString();
-    }
-
-    virtual void print() const {
-        std::cout << toString();
-    }
-
 protected:
     UnitConverter* unit_ptr;
 };
